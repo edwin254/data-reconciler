af77ea9c27b21070196be0f10eb3241ca7f041ee
Edwin Kamande <kamande254@gmail.com>
2024-05-30 01:26:51 +0300

    initialize request views

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from typing import Union
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"id": id}
+    )
